0aa83fec452f592073064b58018c4ba0bcad15fe
Ratob <nashkihaysnairfailfardammahum@gmail.com>
2022-09-13 21:00:14 +0800

    Fixed the examples of async await

--- tesja/test/widget_test.dart
+++ tesja/test/widget_test.dart
@@ -1,8 +1,15 @@
 void main() async {
   try {
-    String order = await getOrder(6, 'Mocha');
+    String order = await getOrder(5, 'Mocha');
     print(order);
   } catch (e) {
+    print(e);
+  }
+
+  try {
+    String seat = await findSeat();
+    print(seat);
+  } catch(e) {
     print(e);
   }
 }
@@ -16,6 +23,21 @@ Future<String> getOrder(int orderAmount, String orderName) {
         return 'You ordered $orderAmount $orderName';
       } else {
         throw 'The stock is unavailable';
+      }
+    },
+  );
+}
+
+Future<String> findSeat() {
+  return Future.delayed(
+    Duration(seconds: 1),
+    () {
+      bool isAvailable = true;
+
+      if (isAvailable) {
+        return 'There\'s a seat right there!';
+      } else {
+        throw 'No seat available';
       }
     },
   );
